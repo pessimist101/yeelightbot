23914ec09b0288cd2647ec2444c664d7dc47e70b
Pessimist101 <issythegirl@gmail.com>
2020-05-10 02:41:11 +0100

    fixed tuple normalisation error

--- cogs/light.py
+++ cogs/light.py
@@ -155,7 +155,8 @@ class Light(commands.Cog):
                 return
         if (len(arg) == 1) and (not arg[0].isdigit()): # must be colour name
             try:
-                red, green, blue = webcolors.name_to_rgb(arg, spec='css3')
+                listArg = list(arg) # nametorgb attempts to lower arg, so needs to /not/ be a tuple
+                red, green, blue = webcolors.name_to_rgb(listArg, spec='css3')
             except ValueError as err:
                 await ctx.send(err)
                 return
