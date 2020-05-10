1dc5d39af8da2ddb5133c6bbbb00a7258faf6813
Pessimist101 <issythegirl@gmail.com>
2020-05-10 01:53:48 +0100

    Embed

--- cogs/light.py
+++ cogs/light.py
@@ -19,6 +19,17 @@ class Light(commands.Cog):
         print('Light cog online')
         await self.client.get_channel(self.config['logChannel']).send('Yeelight cog ready')
 
+    async def getLightStatus(self, light):
+        bulb = Bulb(self.lights[0]['ip'])
+        bulbProps = bulb.get_properties()
+        return bulbProps
+
+
+    async def lightEventMsg(self, ctx, msg):
+        embed = discord.Embed(description=msg)
+        await ctx.send(embed=embed)
+        return
+
     # Commands
     @commands.command()
     async def off(self, ctx):
@@ -31,7 +42,7 @@ class Light(commands.Cog):
                 return
             names.append(light['name'])
         successMsg = ", ".join(names)
-        await ctx.send(f'Turned off **{successMsg}**')
+        await self.lightEventMsg(ctx, f'Turned off **{successMsg}**')
 
     @commands.command()
     async def on(self, ctx):
@@ -44,7 +55,7 @@ class Light(commands.Cog):
                 return
             names.append(light['name'])
         successMsg = ", ".join(names)
-        await ctx.send(f'Turned on **{successMsg}**')
+        await self.lightEventMsg(ctx, f'Turned on **{successMsg}**')
 
     @commands.command()
     async def toggle(self, ctx):
@@ -57,7 +68,7 @@ class Light(commands.Cog):
                 return
             names.append(light['name'])
         successMsg = ", ".join(names)
-        await ctx.send(f'Toggled **{successMsg}**')
+        await self.lightEventMsg(ctx, f'Toggled **{successMsg}**')
 
     @commands.command()
     async def ct(self, ctx, arg):
@@ -77,7 +88,7 @@ class Light(commands.Cog):
                 return
             names.append(light['name'])
         successMsg = ", ".join(names)
-        await ctx.send(f'Set colour temperature of **{successMsg}** to {str(arg)}')    @commands.command()
+        await self.lightEventMsg(ctx, f'Set colour temperature of **{successMsg}** to {str(arg)}')
 
     @commands.command()
     async def bright(self, ctx, arg):
@@ -98,7 +109,7 @@ class Light(commands.Cog):
                 return
             names.append(light['name'])
         successMsg = ", ".join(names)
-        await ctx.send(f'Set brightness **{successMsg}** to {str(arg)}')
+        await self.lightEventMsg(ctx, f'Set brightness **{successMsg}** to {str(arg)}')
 
     @commands.command()
     async def hsv(self, ctx, *arg):
@@ -123,7 +134,7 @@ class Light(commands.Cog):
                 return
             names.append(light['name'])
         successMsg = ", ".join(names)
-        await ctx.send(f'Set HSV on **{successMsg}**')
+        await self.lightEventMsg(ctx, f'Set HSV on **{successMsg}**')
 
     @commands.command()
     async def colour(self, ctx, *arg):
@@ -158,7 +169,7 @@ class Light(commands.Cog):
                 return
             names.append(light['name'])
         successMsg = ", ".join(names)
-        await ctx.send(f'Set colour to \({red}, {green}, {blue}\) on **{successMsg}**')
+        await self.lightEventMsg(ctx, f'Set colour to \({red}, {green}, {blue}\) on **{successMsg}**')
 
     @commands.command()
     async def disco(self, ctx):
@@ -175,7 +186,7 @@ class Light(commands.Cog):
                 return
             names.append(light['name'])
         successMsg = ", ".join(names)
-        await ctx.send(f'Enabled party mode on **{successMsg}**')
+        await self.lightEventMsg(ctx, f'Enabled party mode on **{successMsg}**')
 
 def setup(client):
     client.add_cog(Light(client))
